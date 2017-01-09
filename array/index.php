f3ad7c5990e81df7ccd8849b28a7d1c7ab79867f
Lilia <geertszelia@gmail.com>
2017-01-09 15:35:36 +0100

    Exercice 1: array (faire un tableau)

---
+++ array/index.php
@@ -0,0 +1,16 @@
+<?php
+	require __DIR__ . '/../array/exercice1.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Array</title>
+</head>
+<body>
+	<b>Exercice 1:</b>
+	<div><?php print_r($mois); ?></div>
+</body>
+</html>
