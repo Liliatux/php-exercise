8592d1729c39f900045fe2ac3d12c2bf89b877a2
Lilia <geertszelia@gmail.com>
2017-01-10 14:13:40 +0100

    Exercice 5: form(nouveau formulaire)

--- form/index.php
+++ form/index.php
@@ -26,5 +26,22 @@
 
 		<input type="submit">
 	</form>
+
+	<b>Exercice 5:</b>
+	<form action="index.php" method="get">
+		<label for="civilite">Civilité</label>
+		<select name="civil" id="civilite">
+			<option value="1">Mme</option>
+			<option value="2">Mr</option>
+		</select>
+
+		<label for="name3">Nom</label>
+		<input type="text" name="name3" id="name3" value="fallsack">
+
+		<label for="firstName3">Prénom</label>
+		<input type="text" name="firstName3" id="firstName3" value="Nasus">
+
+		<input type="submit">
+	</form>
 </body>
 </html>
