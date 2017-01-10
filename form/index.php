34509a8191f80d7a6f4255e759fa13eb89cbb723
Lilia <geertszelia@gmail.com>
2017-01-10 15:00:13 +0100

    Exercice 7: form(envoie d'un fichier)

--- form/index.php
+++ form/index.php
@@ -55,15 +55,40 @@
 				</select>
 
 				<label for="name3">Nom</label>
-				<input type="text" name="name3" id="name3">
+				<input type="text" name="name3" id="name3" value="fallsack">
 
 				<label for="firstName3">Prénom</label>
-				<input type="text" name="firstName3" id="firstName3">
+				<input type="text" name="firstName3" id="firstName3" value="Nasus">
 
 				<input type="submit">
 			</form>
 		<?php } else {
 			echo $_GET['civil'].' '.$_GET['name3'].' '.$_GET['firstName3'];
+		} ?>
+	</div>
+	<b>Exercice 7:</b>
+	<div>
+		<?php if(empty($_POST)){ ?>
+			<form action="index.php" method="post">
+				<label for="civilite">Civilité</label>
+				<select name="civil" id="civilite">
+					<option value="1">Mme</option>
+					<option value="2">Mr</option>
+				</select>
+
+				<label for="name3">Nom</label>
+				<input type="text" name="name3" id="name3" value="fallsack">
+
+				<label for="firstName3">Prénom</label>
+				<input type="text" name="firstName3" id="firstName3" value="Nasus">
+
+				<label for="file">Envoyer un fichier</label>
+				<input type="file" name="file" id="file">
+
+				<input type="submit">
+			</form>
+		<?php } else {
+			echo $_POST['civil'].' '.$_POST['name3'].' '.$_POST['firstName3'].' '.$_POST['file'];
 		} ?>
 	</div>
 </body>
