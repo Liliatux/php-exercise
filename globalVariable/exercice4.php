381572d9a5eb9fc334f7f3d9bee60650af1bc37c
Lilia <geertszelia@gmail.com>
2017-01-11 11:32:14 +0100

    Global Variable: exo 4(page qui récupère les cookie)

---
+++ globalVariable/exercice4.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exo4</title>
+</head>
+<body>
+	<?php 
+		echo $_COOKIE['login'].' '.$_COOKIE['mdp'];
+	?>
+</body>
+</html>
