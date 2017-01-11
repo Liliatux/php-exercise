c02fe38e56549050bf865edcf303bf534897f7f2
Lilia <geertszelia@gmail.com>
2017-01-11 15:00:32 +0100

    Date: exo6(nombre de jour en février)

---
+++ date/exercice6.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exo6</title>
+</head>
+<body>
+	<?php 
+		echo cal_days_in_month(CAL_GREGORIAN, 2, 2016);
+		 
+	?>
+</body>
+</html>
