48d4bc76680c123a68c5855d300fd418ae9fcb0e
Lilia <geertszelia@gmail.com>
2017-01-11 12:10:06 +0100

    Date: exo1(format jj/mm/YYYY)

---
+++ date/exercice1.php
@@ -0,0 +1,13 @@
+<?php date_default_timezone_set('UTC'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exo1</title>
+</head>
+<body>
+	<?php echo date("j/m/Y"); ?>
+
+</body>
+</html>
