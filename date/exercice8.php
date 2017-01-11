f915fdf6e4c391b22316a570071136417f73b43d
Lilia <geertszelia@gmail.com>
2017-01-11 15:27:05 +0100

    Date: exo8(date moins 22jours)

---
+++ date/exercice8.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exo8</title>
+</head>
+<body>
+	<?php 
+		echo date('d-m-Y', strtotime('11-01-2017 -20 days'));	 
+	?>
+</body>
+</html>
