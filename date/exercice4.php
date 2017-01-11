9c44bc9f9d2206ebdf1933fc03b3247f5d4522b7
Lilia <geertszelia@gmail.com>
2017-01-11 14:16:24 +0100

    Date: exo4(ajout d'un timestamp)

---
+++ date/exercice4.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exo4</title>
+</head>
+<body>
+	<?php 
+		$date = new DateTime();
+		echo $date -> getTimestamp();
+	?>
+
+</body>
+</html>
