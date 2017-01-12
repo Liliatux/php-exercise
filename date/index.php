9f84a86ab8f7fd437da17082d94a7edbbf0f100a
Lilia <geertszelia@gmail.com>
2017-01-12 13:31:26 +0100

    Ajout des mois, années et jours de la semaine

--- date/index.php
+++ date/index.php
@@ -1,3 +1,8 @@
+<?php 
+	$month = array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+	$day = array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,26 +11,42 @@
 </head>
 <body>
 	<h1>Calendrier</h1>
+	<?php echo $month[""]; ?>
 	<form action="index.php" method="post">
 		<label for="month">Mois</label>
 		<select name="month" id="month">
-			<option value="Janvier">Janvier</option>
+			<?php 
+				foreach ($month as $value) {
+					echo "<option value='$value'>$value</option>";
+				}
+			?>
 		</select>
 		<label for="year">Année</label>
 		<select name="year" id="year">
-			<option value="2017">2017</option>
+			<?php 
+				for ($year = 2017; $year >= 1970; $year--) {
+					echo "<option value='$year'>$year</option>";
+				}
+			?>
 		</select>
 		<input type="submit">
 	</form>
+	<div class="choice">
+		<?php echo $_POST['month'].' '.$_POST['year'] ?>
+	</div>
 	<table>
 		<thead>
-			<tr><?php echo $_POST['month'].' '.$_POST['year'] ?></tr>
+			<tr>
+				<?php foreach ($day as $value): ?>
+					<td><?php echo $value; ?></td>					
+				<?php endforeach; ?>
+			</tr>
 		</thead>
 		<tbody>
-			//ligne
 			<tr>
-				//colone
-				<td></td>
+				<?php foreach ($day as $key => $value): ?>
+						<td><?php echo $key; ?></td>
+				<?php endforeach; ?>				
 			</tr>
 		</tbody>
 	</table>
